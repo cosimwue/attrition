77fd79c3907de578e60a0206b625444dbdc5a0d7
severinsimmler <severin.simmler@posteo.de>
2018-06-27 08:46:00 +0200

    chore: add print

--- deterministic.py
+++ deterministic.py
@@ -1,8 +1,10 @@
 def square_law(r, b, t, a, c):
+    r1 = r
+    b1 = b
     for i in range(t):
+        print("{} vs. {}".format(round(r), round(b)))
         r = r - c * b
         b = b - a * r
         if r <= 0 or b <= 0:
             break
     return r, b
-
