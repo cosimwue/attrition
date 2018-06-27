1b994575e5b0752b5bad31cbcccf6e47ffb5be56
severinsimmler <severin.simmler@posteo.de>
2018-06-27 08:38:09 +0200

    chore: add break

--- deterministic.py
+++ deterministic.py
@@ -1,5 +1,9 @@
 def square_law(r, b, t, a, c):
     for i in range(t):
+        print(r)
         r = r - c * b
         b = b - a * r
+        if r <= 0 or b <= 0:
+            break
     return r, b
+
