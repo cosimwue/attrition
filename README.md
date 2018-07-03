44243725c27d1f610d72a619c468d4e26d9fd843
severinsimmler <severin.simmler@posteo.de>
2018-07-03 23:49:10 +0200

    feat: add argparse

---
+++ README.md
@@ -0,0 +1,50 @@
+# Attrition Warfare
+
+```
+$ python warsim_max_2v2.py --help
+usage: warsim_max_2v2.py [-h] [--size-r1 SIZE_R1] [--power-r1 POWER_R1]
+                         [--size-b1 SIZE_B1] [--power-b1 POWER_B1]
+                         [--time TIME] [--coeff-r1 COEFF_R1]
+                         [--coeff-b1 COEFF_B1] [--size-r2 SIZE_R2]
+                         [--power-r2 POWER_R2] [--coeff-r2 COEFF_R2]
+                         [--size-b2 SIZE_B2] [--power-b2 POWER_B2]
+                         [--coeff-b2 COEFF_B2]
+
+Simulates the course of a battle after Osipov's example. Uses amount and power
+of each unit for both sides (currently maximum of two different units per
+side) to generate a probability of death for each unit, then rolls using
+possible coefficients to determine the outcome. Repeats per given time unit,
+or until one side has 0 units left.
+
+optional arguments:
+  -h, --help           show this help message and exit
+  --size-r1 SIZE_R1    Describes the size of the first part of the units on
+                       side r.
+  --power-r1 POWER_R1  Describes the power of the first part of the units on
+                       side r.
+  --size-b1 SIZE_B1    Describes the size of the first part of the units on
+                       side b.
+  --power-b1 POWER_B1  Describes the power of the first part of the units on
+                       side b.
+  --time TIME          Describes the amount of battle rounds/time units.
+  --coeff-r1 COEFF_R1  Describes bonus/malus (armour, training, position) of a
+                       unit of the first part of side r (maximum = 1, default
+                       = 0).
+  --coeff-b1 COEFF_B1  Describes bonus/malus (armour, training, position) of a
+                       unit of the first part of side r (maximum = 1, default
+                       = 0).
+  --size-r2 SIZE_R2    Describes the size of the second part of the units on
+                       side r (default = 0).
+  --power-r2 POWER_R2  Describes the power of the second part of the units on
+                       side r (default = 0).
+  --coeff-r2 COEFF_R2  Describes bonus/malus (armour, training, position) of a
+                       unit of the second part of side r (maximum = 1, default
+                       = 0).
+  --size-b2 SIZE_B2    Describes the size of the second part of the units on
+                       side b (default = 0).
+  --power-b2 POWER_B2  Describes the power of the second part of the units on
+                       side b (default = 0).
+  --coeff-b2 COEFF_B2  Describes bonus/malus (armour, training, position) of a
+                       unit of the second part of side b (maximum = 1, default
+                       = 0).
+```
--- warsim_max_2v2.py
+++ warsim_max_2v2.py
@@ -3,16 +3,17 @@
 Created on Mon July 02 09:06:15 2018
 
 @author: RBabl
-
-Simulates the course of a battle after Osipov's example.
-Uses amount and power of each unit for both sides (currently maximum of two different units per side)
-to generate a probability of death for each unit, then rolls using possible coefficients to determine the outcome. 
-Repeats per given time unit, or until one side has 0 units left.  
-
 """
+
+DESCRIPTION = """Simulates the course of a battle after Osipov's example. 
+              Uses amount and power of each unit for both sides (currently maximum of two different units per side) 
+              to generate a probability of death for each unit, then rolls using possible coefficients to determine the outcome. 
+              Repeats per given time unit, or until one side has 0 units left.
+              """
 
 import numpy as np
 import random as rd
+import argparse
 
 def _prob(size_def, size_one, power_one, size_two = 0 , power_two = 0):
     """Generates the probability for an individual unit to succumb to enemy fire.
@@ -87,6 +88,25 @@ def warsim(size_r1, power_r1, size_b1, power_b1, time, coeff_r1 = 0, coeff_b1 = 
         if r == 0 or b == 0:
             break
     print("Size of r per time unit: ", r_size,"Size of b per time unit: ",b_size)
-    
-if __name__ == '__main__':
-    warsim(500,0.02,1000,0.01,100)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description=DESCRIPTION)
+
+    parser.add_argument("--size-r1", type=int, help="Describes the size of the first part of the units on side r.")
+    parser.add_argument("--power-r1", type=int, help="Describes the power of the first part of the units on side r.")
+    parser.add_argument("--size-b1", type=int, help="Describes the size of the first part of the units on side b.")
+    parser.add_argument("--power-b1", type=int, help="Describes the power of the first part of the units on side b.")
+    parser.add_argument("--time", type=int, help="Describes the amount of battle rounds/time units.")
+    parser.add_argument("--coeff-r1", type=float, default=.0, help="Describes bonus/malus (armour, training, position) of a unit of the first part of side r (maximum = 1, default = 0).")
+    parser.add_argument("--coeff-b1", type=float, default=.0, help="Describes bonus/malus (armour, training, position) of a unit of the first part of side r (maximum = 1, default = 0).")
+    parser.add_argument("--size-r2", type=int, default=0, help="Describes the size of the second part of the units on side r (default = 0).")
+    parser.add_argument("--power-r2", type=int, default=0, help="Describes the power of the second part of the units on side r (default = 0).")
+    parser.add_argument("--coeff-r2", type=float, default=.0, help="Describes bonus/malus (armour, training, position) of a unit of the second part of side r (maximum = 1, default = 0).")
+    parser.add_argument("--size-b2", type=int, default=0, help="Describes the size of the second part of the units on side b (default = 0).")
+    parser.add_argument("--power-b2", type=int, default=0, help="Describes the power of the second part of the units on side b (default = 0).")
+    parser.add_argument("--coeff-b2", type=float, default=.0, help="Describes bonus/malus (armour, training, position) of a unit of the second part of side b (maximum = 1, default = 0).")
+
+    args = parser.parse_args()
+
+    warsim(args.size_r1, args.power_r1, args.size_b1, args.power_b1, args.time, args.coeff_r1, args.coeff_b1,
+           args.size_r2, args.power_r2, args.coeff_r2, args.size_b2, args.power_b2, args.coeff_b2)
